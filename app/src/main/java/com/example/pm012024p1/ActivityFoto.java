8009b76412bccfed96b62a5d87dcb962ac450bd2
Brayan Matute <hazabaraho981@gmail.com>
2024-02-15 09:52:55 -0600

    funcion compartir correcta

--- app/src/main/java/com/example/pm012024p1/ActivityFoto.java
+++ app/src/main/java/com/example/pm012024p1/ActivityFoto.java
@@ -10,6 +10,8 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -17,23 +19,26 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+
 public class ActivityFoto extends AppCompatActivity {
 
     static final int peticion_camara = 100;
-    static final int  peticion_foto = 102;
+    static final int peticion_foto = 102;
     String FotoPath;
     ImageView imageView;
     Button btntakefoto;
 
+    Button compartirfoto;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foto);
 
-        imageView = (ImageView) findViewById(R.id.imageView);
-        btntakefoto = (Button)  findViewById(R.id.btntakefoto);
+        imageView = findViewById(R.id.imageView);
+        btntakefoto = findViewById(R.id.btntakefoto);
+        compartirfoto = findViewById(R.id.btncompartirfoto);
 
         btntakefoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,30 +46,50 @@ public class ActivityFoto extends AppCompatActivity {
                 Permisos();
             }
         });
+
+        compartirfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compartirFoto();
+            }
+        });
     }
 
-    private void Permisos()
-    {
-        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED)
-        {
+    private void Permisos() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},
                     peticion_camara);
-        }
-        else
-        {
+        } else {
             tomarfoto();
         }
     }
 
-    private void tomarfoto()
-    {
+    private void tomarfoto() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        if(intent.resolveActivity(getPackageManager()) != null)
-        {
+        if (intent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(intent, peticion_foto);
         }
+    }
+
+    private void compartirFoto() {
+        if (imageView.getDrawable() != null) {
+            Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_STREAM, getImageUri(bitmap));
+            startActivity(Intent.createChooser(intent, "Compartir imagen"));
+        } else {
+            Toast.makeText(getApplicationContext(), "Toma una foto antes de compartir", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private Uri getImageUri(Bitmap bitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "Title", null);
+        return Uri.parse(path);
     }
 
     @Override
@@ -72,28 +97,23 @@ public class ActivityFoto extends AppCompatActivity {
     int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(requestCode == peticion_camara)
-        {
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-            {
+        if (requestCode == peticion_camara) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 tomarfoto();
-            }
-            else
-            {
+            } else {
                 Toast.makeText(getApplicationContext(), "Permiso denegado", Toast.LENGTH_LONG).show();
             }
         }
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == peticion_foto && resultCode == RESULT_OK)
-        {
+        if (requestCode == peticion_foto && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imagen = (Bitmap) extras.get("data");
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
             imageView.setImageBitmap(imagen);
         }
     }
